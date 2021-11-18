60a627fc4acc739558f2de1fd2c3c04858c998b0
rafael-shimada <rafael.shimada17@gmail.com>
2021-11-17 21:33:23 -0300

    first commit

---
+++ src/main/kotlin/com/example/kotlinspring01/KotlinSpring01Application.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspring01
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpring01Application
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpring01Application>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspring01/KotlinSpring01ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspring01
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpring01ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
